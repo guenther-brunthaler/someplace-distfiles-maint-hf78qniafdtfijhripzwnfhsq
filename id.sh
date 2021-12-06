35ef5054cab81e9a743ce10e9e4d7cb7c001dd1e
Guenther Brunthaler <gb@emgenxx69lwyn5ctlr4nl64ul.local>
2021-12-06 17:21:06 +0000

    Changes

--- id.sh
+++ id.sh
@@ -1,9 +1,9 @@
 #! /bin/sh
 # Calculate a custom data identifier of data read from standard input.
 #
-# The identifier consists of the data size, followed by an equals sign ("=")
+# The identifier consists of the data size, followed by an underscore ("_")
 # and a CRC or the data. A CRC-32 as calculated by "cksum" is used as the CRC.
 #
-# v2021.340.2
+# v2021.340.3
 
-cksum | sed 's/\(.*\) \(.*\)/\2=\1/'
+cksum | sed 's/\(.*\) \(.*\)/\2_\1/'
